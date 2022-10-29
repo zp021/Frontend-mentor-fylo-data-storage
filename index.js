4fd07084f9a58aac16ac1f686474e4d652529d02
zp021 <111681672+zp021@users.noreply.github.com>
2022-10-29 07:55:51 +0200

    working storage countup commit

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const storageMeter = document.querySelector(".storage-meter")
 const storageLeft = document.querySelector(".storage-left")
+const storageUsed = document.getElementById("storage-used")
 const storageNumber = document.getElementById("left")
 const point = document.querySelector(".point")
 
 const storage = storageLeft.dataset.number;
+const used = storageUsed.dataset.number;
 window.getComputedStyle(storageMeter).height;
 
 window.onload = function() {
     storageMeter.style.width = storageMeter.dataset.width;
     point.style.left = point.dataset.position;
+
+    let gbUsed = setInterval(() => {
+        storageUsed.textContent++;
+        if(storageUsed.textContent == used) {
+            clearInterval(gbUsed)
+        }
+    })
+
     let gbCount = setInterval(() => {
         storageNumber.textContent++;
         if(storageNumber.textContent == storage){
@@ -16,3 +26,7 @@ window.onload = function() {
         }
     },10)
 }
+
+
+
+
